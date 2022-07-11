2b3527c8c7b5382dba047d7f94f2da0716a74a85
1997926hh <762100246@qq.com>
2022-07-11 11:34:58 +0800

    0.0.15

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 aquarius-ui-vue for Vue 3.0
 
-## Install
+## Document 文档
+http://www.signsplan.com/ariesUI
+
+## Install 安装
 ```shell
 npm install aries-ui -S
 ```
 
 
-## Quick Start
+## Quick Start 启动
 ``` javascript
 import Vue from 'vue'
 import aqrUI from 'aries-ui'
--- packages/components/tabs/src/tabs.ts
+++ packages/components/tabs/src/tabs.ts
@@ -1,8 +1,8 @@
-import { ref, defineComponent, readonly, h, provide, ExtractPropTypes, reactive, computed, watch, onMounted, } from "vue";
+import { ref, defineComponent, readonly, h, provide, ExtractPropTypes, reactive, computed, watch, onMounted, RendererNode, RendererElement, VNodeArrayChildren, } from "vue";
 import type { VNode } from "vue"
 import { TabsProvide } from "./types";
 import { _tabsProps } from "./props"
-import { cssName, isUndefined, guid } from "aries-ui/libs";
+import { cssName, isUndefined, guid, throwError } from "aries-ui/libs";
 import { onClickOutside } from "@vueuse/core";
 import tabsBar from "./tabs-bar.vue"
 const COMPONENT_NAME = "AriTabs";
@@ -48,6 +48,7 @@ export default defineComponent({
         const barWidth = ref(props.barWidth)
         //如果slots.default()有值则赋予slot
         const slot = slots.default?.() ?? []
+        const usefulSlots = reactive<any[]>([])
         //标签页宽度
         let itemWidth = ref(0);
         //滚动条离左边的默认宽度
@@ -55,7 +56,6 @@ export default defineComponent({
         //
         let barLeft = ref(0)
         const tabsID = guid()
-
         //method
         const handleTabsItemClick: TabsProvide['handleTabsItemClick'] = (tabsItem) => {
             activeIndex.value = tabsItem.index
@@ -65,30 +65,34 @@ export default defineComponent({
             return (dom as HTMLElement).offsetLeft
         }
         const updateBarLeft = (): void => {
-            let itemIndex = 0
-
             if (activeIndex.value === "") {
-                const index = slot[0].props?.index
-                barLeft.value = getDomOffsetLeft(index)
+                if (usefulSlots.length > 0) {
+                    const firstSlot = usefulSlots[0]
+                    const index: number | string = firstSlot.props.index
+                    activeIndex.value = index
+                    barLeft.value = getDomOffsetLeft(index)
+                }
             }
             else {
-
-            }
-            slot.map((item, index) => {
-                if (item.props?.index === activeIndex.value) {
-                    itemIndex = index
-                    barLeft.value = getDomOffsetLeft(activeIndex.value)//itemWidth.value * itemIndex
+                if (usefulSlots.length > 0) {
+                    usefulSlots.map((item,index) => {
+                        if (item.props?.index === activeIndex.value) {
+                            setBarLeft(activeIndex.value)
+                        }
+                    })
                 }
-            })
-        }
+            }
 
+        }
+        const setBarLeft = (index: number | string) => {
+            barLeft.value = getDomOffsetLeft(index)//itemWidth.value * itemIndex
+        }
         //初始化宽度
         const initWidth = () => {
-            const childrens = slots.default?.() ?? []
-            if (childrens.length > 0) {
+            if (usefulSlots.length > 0) {
                 let maxLength = 0
-                childrens.map((children) => {
-                    const label = children.props?.label ?? ""
+                usefulSlots.map((item) => {
+                    const label = item.props?.label ?? ""
                     const labelLength = label.length
                     maxLength = maxLength > labelLength ? maxLength : labelLength
                 })
@@ -101,9 +105,42 @@ export default defineComponent({
                 barInitLeft.value = Math.abs(itemWidth.value - barWidth.value) / 2
             }
         }
+        const initUsefulSlots = () => {
+            slot.map((item, index) => {
+                if (Array.isArray(item.children)) {
+                    const childrenSlots = item.children
+                    if (childrenSlots.length > 0) {
+                        setUsefulSlots(childrenSlots)
+                    }
+                }
+                if (item.props?.index != undefined) {
+                    usefulSlots.push(item)
+                }
+            })
+            if (usefulSlots.length == 0) {
+                throwError(
+                    COMPONENT_NAME,
+                    `请添加index属性`
+                );
+            }
+        }
+        const setUsefulSlots = (prsentSlots: VNodeArrayChildren) => {
+            prsentSlots.map(item => {
+                const childrenSlots = (item as VNode<RendererNode, RendererElement>).children
+                if (Array.isArray(childrenSlots)) {
+                    if (childrenSlots.length > 0) {
+                        setUsefulSlots(childrenSlots)
+                    }
+                }
+                if ((item as VNode<RendererNode, RendererElement>).props?.index != undefined) {
+                    usefulSlots.push(item)
+                }
+            })
+        }
 
         //
         onMounted(() => {
+            initUsefulSlots()
             updateBarLeft()
             initWidth()
         })
